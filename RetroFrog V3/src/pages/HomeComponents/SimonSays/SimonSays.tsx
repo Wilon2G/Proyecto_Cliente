f8b2c918d0700ef15ae3c19063bacb3eebfd7c7d
Guillermo <guilleleongordo@gmail.com>
2024-10-27 11:51:54 +0100

    Arreglandoun poco el simon SimonSays

--- RetroFrog V3/src/pages/HomeComponents/SimonSays/SimonSays.tsx
+++ RetroFrog V3/src/pages/HomeComponents/SimonSays/SimonSays.tsx
@@ -1,39 +1,43 @@
 export const SimonSays = () => {
-  return <>
-  <div className="simonSays">
-  <div className="container-gameboy">
-        <div id="outer-circle">
+  return (
+    <>
+      <div className="simonSays">
+        
+        <div className="buttons-gameboy">
+        <div className="container-gameboy">
+          <div id="outer-circle">
             <div id="topleft"></div>
             <div id="topright"></div>
             <div id="bottomleft"></div>
             <div id="bottomright"></div>
             <div id="inner-circle">
-                <div id="title" className="font-effect-emboss">SIMON!</div>
-                <div id="switches">
-                    <input type="checkbox" className="toggle" id="on"/>
-                    <button className="button" id="start">Start</button>
-                    <input type="checkbox" className="toggle" id="strict"/>
-                </div>
-                <div className="text1">
-                    <span>POWER</span><span>STRICT</span>
-                </div>
-                <div id="turn"></div>
-                <div className="text2">
-                    COUNT
-                </div>
+              <div id="title" className="font-effect-emboss">
+                SIMON!
+              </div>
+              <div id="switches">
+                <input type="checkbox" className="toggle" id="on" />
+                <button className="button" id="start">
+                  Start
+                </button>
+                <input type="checkbox" className="toggle" id="strict" />
+              </div>
+              <div className="text1">
+                <span>POWER</span>
+                <span>STRICT</span>
+              </div>
+              <div id="turn"></div>
+              <div className="text2">COUNT</div>
             </div>
+          </div>
         </div>
-    </div>
-    <div className="buttons-gameboy">
-        <button className="up-down-directions"></button>
-        <button className="right-left-directions"></button>
-        <button className="button-action-1"></button>
-        <button className="button-action-2"></button>
-    </div>
+          <button className="up-down-directions"></button>
+          <button className="right-left-directions"></button>
+          <button className="button-action-1"></button>
+          <button className="button-action-2"></button>
+        </div>
 
-    <script src="js/index.js"></script>
-  
-    </div>
-  
-  </>;
+        <script src="js/index.js"></script>
+      </div>
+    </>
+  );
 };
