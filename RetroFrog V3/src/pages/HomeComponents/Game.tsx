6cb86cc08649d39b05eb75fa0ca008fca5576512
Favio138 <44302490+Favio138@users.noreply.github.com>
2024-11-04 15:45:11 +0100

    [fix] JS game BackCover color

--- RetroFrog V3/src/pages/HomeComponents/Game.tsx
+++ RetroFrog V3/src/pages/HomeComponents/Game.tsx
@@ -16,7 +16,11 @@ export default function GameBox({game,setOnPlay}:GameBoxProps) {
       <div className="box">
                 <div className="shadow"></div>
                 <div className="box__face box__face--front " style={{backgroundImage: urlgame}}></div>
-                <div className="box__face box__face--back " style={{backgroundImage: urlbackgame }}><h3>{game.title}</h3><p>{game.description}</p><PlayButton setOnPlay={setOnPlay} /></div>
+                <div className="box__face box__face--back " 
+                  style={game.backroute==""?{backgroundColor: game.color}:{backgroundImage: urlbackgame }}>
+                  <h3>{game.title}</h3>
+                  <p>{game.description}</p>
+                  <PlayButton setOnPlay={setOnPlay} /></div>
                 <div className="box__face box__face--right " style={{backgroundColor: game.color }}><div>Game-Poy Color</div></div>
                 
               </div>
