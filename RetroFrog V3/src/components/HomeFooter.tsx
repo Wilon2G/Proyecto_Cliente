39da6a4520ec776d6fc0b6c24ff71de17964dbbb
Guillermo <guilleleongordo@gmail.com>
2024-11-06 20:03:29 +0100

    Añadido footer

---
+++ RetroFrog V3/src/components/HomeFooter.tsx
@@ -0,0 +1,32 @@
+
+export default function HomeFooter(){
+    return <footer>
+        <h1>¡Gracias por visitar RetrFrog!</h1>
+        <div>
+            <h3>Contacta con nosotros</h3>
+            <ul>
+                <li>Phone: +34 916036188</li>
+                <li>¿Cómo podemos ayudarte?</li>
+                <li>¡No te pierdas lo último, activa nuestro newsletter!</li>
+            </ul>
+        </div>
+        <div>
+        <h3>Nuestras redes</h3>
+            <ul>
+                <li><img src="/src/assets/icons/X.png"></img></li>
+                <li>¿Cómo podemos ayudarte?</li>
+                <li>¡No te pierdas lo último, activa nuestro newsletter!</li>
+            </ul>
+        </div>
+        <div>
+        <h3>Contacta con nosotros</h3>
+            <ul>
+                <li>Phone: +34 916036188</li>
+                <li>¿Cómo podemos ayudarte?</li>
+                <li>¡No te pierdas lo último, activa nuestro newsletter!</li>
+            </ul>
+        </div>
+    </footer>
+}
+
+{/* <a target="_blank" href="https://icons8.com/icon/YfCbGWCWcuar/twitterx">X</a> icono de <a target="_blank" href="https://icons8.com">Icons8</a> */}
