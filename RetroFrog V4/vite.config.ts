73dbe794ded472021f4f260278074ab043e8bb08
gonzalo <gonzalos.camara@gmail.com>
2024-12-09 12:46:53 +0100

    Server quitado

--- RetroFrog V4/vite.config.ts
+++ RetroFrog V4/vite.config.ts
@@ -1,9 +1,9 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import fs from "fs";
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import fs from 'fs';
 
-declare module "@remix-run/node" {
+declare module '@remix-run/node' {
   interface Future {
     v3_singleFetch: true;
   }
@@ -22,7 +22,4 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
-  server: {
-    host: "Retrofrog.com",
-  },
 });
