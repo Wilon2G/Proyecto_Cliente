835ca02e8e457d2a59fc8c2c01c759737192daf7
guillermo <guilleleongordo@gmail.com>
2025-01-13 13:20:05 +0100

    Cambio provisional de validacion zod (kk)

--- RetroFrog V4/app/utils/zodValidation.ts
+++ RetroFrog V4/app/utils/zodValidation.ts
@@ -2,6 +2,6 @@ import { z } from "zod";
 
 
 export const logInSchema = z.object({
-    userName: z.string().nonempty({message:"You must fill the User Name field"}).email({ message: "Invalid User Name" }),
+    userName: z.string().nonempty({message:"You must fill the User Name field"}),
     password: z.string().nonempty({message:"You must fill the Password field"}).min(5, { message: "Password must have at least five characters" }),
 });
