85cb2ef97556a36b564ea71cc5328788bd68c00f
Guillermo <guilleleongordo@gmail.com>
2025-01-14 20:19:43 +0100

    added register schema

--- RetroFrog V4/app/utils/zodSchemas.ts
+++ RetroFrog V4/app/utils/zodSchemas.ts
@@ -5,3 +5,10 @@ export const logInSchema = z.object({
     userName: z.string().min(1,  "You must fill the User Name field" ),
     password: z.string().nonempty({message:"You must fill the Password field"}).min(5, { message: "Password must have at least five characters" }),
 });
+
+export const registerSchema = z.object({
+    userName: z.string().min(1,  "You must fill the User Name field" ),
+    password: z.string().nonempty({message:"You must fill the Password field"}).min(5, { message: "Password must have at least five characters" }),
+    name: z.string().min(1, "You must fill this field"),
+});
+
