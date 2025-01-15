3271b3d50ade40c97e5effbef7d537f6e606187a
Favio138 <44302490+Favio138@users.noreply.github.com>
2025-01-15 09:47:34 +0100

    V1-Routing-Convention

--- RetroFrog V4/app/routes/home/main.tsx
+++ RetroFrog V4/app/routes/home/main.tsx
@@ -1,4 +1,4 @@
-import News from "../components/News";
+import News from '../../components/News';
 
 export default function HomePage() {
   return (
@@ -6,12 +6,11 @@ export default function HomePage() {
       <header>
         <h1>Welcome [aquí el nombre de usuario] !</h1>
       </header>
-      <main className='grid grid-cols-2 gap-4'>
-        <News/>
+      <main className="grid grid-cols-2 gap-4">
+        <News />
         <div className="bg-orange-400">
           <h1>CkeckOut our latest sales!</h1>
         </div>
-
       </main>
     </div>
   );
