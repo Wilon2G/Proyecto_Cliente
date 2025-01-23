45d131c5600d7bfe4616d79bdba2b50b74b06c5d
“Gonzalo” <gonzalos.camara@gmail.com>
2025-01-23 19:12:57 +0100

    USERNAME DE NUEVO

--- RetroFrog V4/app/models/user.server.ts
+++ RetroFrog V4/app/models/user.server.ts
@@ -1,70 +1,70 @@
-import db from "~/db.server";
+import db from '~/db.server';
 
-
-export async function checkUser(username:string, password:string){
-    const user= await db.user.findUnique({
-        where: {
-            username:username
-        }
-    })
-    if (!user) {
-        return null;
-    }
-    if (user.password===password) {
-        return user.id;
-    }
-    else{
-        return null;
-    }
+export async function checkUser(username: string, password: string) {
+  const user = await db.user.findUnique({
+    where: {
+      userName: username,
+    },
+  });
+  if (!user) {
+    return null;
+  }
+  if (user.password === password) {
+    return user.id;
+  } else {
+    return null;
+  }
 }
 
-export async function userExists(username:string){
-    const user= await db.user.findUnique({
-        where:{
-            username:username
-        }
-    })
-    if (!user) {
-        return false;
-    }
-    else{
-        return true;
-    }
+export async function userExists(username: string) {
+  const user = await db.user.findUnique({
+    where: {
+      userName: username,
+    },
+  });
+  if (!user) {
+    return false;
+  } else {
+    return true;
+  }
 }
-
 
 export function getUserById(id: string) {
-    return db.user.findUnique({
-      where: {
-        id,
-      },
-    });
-  }
-  
-export async function updateTheme(id:string,theme:string,bg:string,font:string){
-    const user= await getUserById(id);
-    const userid=user?.id;
-    const updatedTheme = `${theme}:${bg}:${font}`;
-
-    // Actualiza el tema en la base de datos
-    return db.user.update({
-      where: { id:userid },
-      data: {
-        theme: updatedTheme, // Asumiendo que 'theme' es un campo de tipo string en tu modelo
-      },
-    });
+  return db.user.findUnique({
+    where: {
+      id,
+    },
+  });
 }
 
-export async function getThemes(id:string){
-    const user= await getUserById(id);
-    const userid=user?.id;
-    const themeData= await db.user.findUnique({
-        where: { id:userid },
-        select:{
-            theme:true
-        }
-    })
-    
-    return  themeData?.theme.split(":");
+export async function updateTheme(
+  id: string,
+  theme: string,
+  bg: string,
+  font: string,
+) {
+  const user = await getUserById(id);
+  const userid = user?.id;
+  const updatedTheme = `${theme}:${bg}:${font}`;
 
+  // Actualiza el tema en la base de datos
+  return db.user.update({
+    where: { id: userid },
+    data: {
+      theme: updatedTheme, // Asumiendo que 'theme' es un campo de tipo string en tu modelo
+    },
+  });
+}
+
+export async function getThemes(id: string) {
+  const user = await getUserById(id);
+  const userid = user?.id;
+  const themeData = await db.user.findUnique({
+    where: { id: userid },
+    select: {
+      theme: true,
+    },
+  });
+
+  return themeData?.theme.split(':');
 }
