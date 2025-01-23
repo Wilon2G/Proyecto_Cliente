c6e976606c326321521b88705756de7e38aea17c
Favio138 <44302490+Favio138@users.noreply.github.com>
2025-01-24 00:41:56 +0100

    style merge

--- RetroFrog V4/app/routes/home.tsx
+++ RetroFrog V4/app/routes/home.tsx
@@ -1,5 +1,5 @@
 import { LoaderFunction } from '@remix-run/node';
-import { NavLink, Outlet, useLoaderData } from '@remix-run/react';
+import { NavLink, Outlet } from '@remix-run/react';
 import { useState } from 'react';
 import {
   CollapseArrow,
@@ -14,6 +14,7 @@ import {
 } from '~/components/IconsSVG';
 import MusicPlayer from '~/components/MusicPlayer';
 import { getSession } from '~/sessions';
+import prisma from '~/utils/prismaClient';
 
 export let loader: LoaderFunction = async ({ request }) => {
   const cookieHeader = request.headers.get('cookie');
@@ -21,24 +22,18 @@ export let loader: LoaderFunction = async ({ request }) => {
 
   const theme = session.get('theme') || 'dark';
 
-  return { theme };
+  const games = await prisma.game.findMany();
+  return { games, theme };
 };
+
 export default function HomePage() {
   const [isCollapsed, setIsCollapsed] = useState(true);
   const [musicState, setMusicState] = useState(false);
 
   // Obtener el tema actual
-  const theme = useLoaderData<{ theme: string }>().theme;
+  const theme = 'dark'; // Aquí deberías obtener dinámicamente el tema, igual que en Shop
 
-  // Definir las clases de fondo y el color de los iconos
-  const colorClasses =
-    theme === 'dark' ? 'text-white' : 'text-[var(--primary)]';
-  const bgClasses =
-    theme === 'dark' ? 'bg-[var(--primary-reverse)]' : 'bg-[var(--primary)]';
-
-  // Definir el color de texto dinámico basado en el tema
-  const textColor =
-    theme === 'dark' ? 'var(--primary-reverse)' : 'var(--primary)';
+  const textClasses = theme === 'dark' ? 'white' : 'black'; // Cambiado a "text-white" para dark theme
 
   function toggleSidebar() {
     setIsCollapsed((prev) => !prev);
@@ -50,13 +45,12 @@ export default function HomePage() {
 
   return (
     <>
-      <aside
-        className={`sidebar ${isCollapsed ? 'collapsed' : ''} ${bgClasses}`}
-      >
+      <aside className={`sidebar ${isCollapsed ? 'collapsed' : ''} bg-primary`}>
         <header className="sidebar-header">
           <button className="toggler" onClick={toggleSidebar}>
-            <span style={{ color: textColor }}>
-              <CollapseArrow iconFill={colorClasses} />
+            <span>
+              <CollapseArrow iconFill={textClasses} />{' '}
+              {/* Se pasa la clase dinámica */}
             </span>
           </button>
           <a href="../home/main" className="header-logo border-primary-reverse">
@@ -72,31 +66,28 @@ export default function HomePage() {
             <li className="nav-item">
               <NavLink to="main" className="nav-link">
                 <span className="nav-icon">
-                  <HomeIcon iconFill={colorClasses} />
+                  <HomeIcon iconFill={textClasses} />{' '}
+                  {/* Se pasa la clase dinámica */}
                 </span>
-                <span className="nav-label" style={{ color: textColor }}>
-                  Home
-                </span>
+                <span className="nav-label">Home</span>
               </NavLink>
             </li>
             <li className="nav-item">
               <NavLink to="shop" className="nav-link">
                 <span className="nav-icon">
-                  <ShopIcon iconFill={colorClasses} />
+                  <ShopIcon iconFill={textClasses} />{' '}
+                  {/* Se pasa la clase dinámica */}
                 </span>
-                <span className="nav-label" style={{ color: textColor }}>
-                  Shop
-                </span>
+                <span className="nav-label">Shop</span>
               </NavLink>
             </li>
             <li className="nav-item">
               <NavLink to="library" className="nav-link">
                 <span className="nav-icon">
-                  <GamesIcon iconFill={colorClasses} />
+                  <GamesIcon iconFill={textClasses} />{' '}
+                  {/* Se pasa la clase dinámica */}
                 </span>
-                <span className="nav-label" style={{ color: textColor }}>
-                  Games
-                </span>
+                <span className="nav-label">Games</span>
               </NavLink>
             </li>
             <li className="nav-item">
@@ -108,21 +99,19 @@ export default function HomePage() {
                 className="nav-link"
               >
                 <span className="nav-icon">
-                  <FavGamesIcon iconFill={colorClasses} />
+                  <FavGamesIcon iconFill={textClasses} />{' '}
+                  {/* Se pasa la clase dinámica */}
                 </span>
-                <span className="nav-label" style={{ color: textColor }}>
-                  Favorites
-                </span>
+                <span className="nav-label">Favorites</span>
               </NavLink>
             </li>
             <li className="nav-item">
               <div className="nav-link" onClick={toggleMusic}>
                 <span className="nav-icon">
-                  <MusicIcon iconFill={colorClasses} />
+                  <MusicIcon iconFill={textClasses} />{' '}
+                  {/* Se pasa la clase dinámica */}
                 </span>
-                <span className="nav-label" style={{ color: textColor }}>
-                  Music
-                </span>
+                <span className="nav-label">Music</span>
               </div>
               <MusicPlayer
                 className={musicState ? 'music-enter' : 'music-exit'}
@@ -133,37 +122,34 @@ export default function HomePage() {
             <li className="nav-item">
               <NavLink to="user" className="nav-link">
                 <span className="nav-icon">
-                  <UserIcon iconFill={colorClasses} />
+                  <UserIcon iconFill={textClasses} />{' '}
+                  {/* Se pasa la clase dinámica */}
                 </span>
-                <span className="nav-label" style={{ color: textColor }}>
-                  Profile
-                </span>
+                <span className="nav-label">Profile</span>
               </NavLink>
             </li>
             <li className="nav-item">
               <NavLink to="settings" className="nav-link">
                 <span className="nav-icon">
-                  <SettingsIcon iconFill={colorClasses} />
+                  <SettingsIcon iconFill={textClasses} />{' '}
+                  {/* Se pasa la clase dinámica */}
                 </span>
-                <span className="nav-label" style={{ color: textColor }}>
-                  Settings
-                </span>
+                <span className="nav-label">Settings</span>
               </NavLink>
             </li>
             <li className="nav-item">
               <NavLink to="/logout" className="nav-link">
                 <span className="nav-icon">
-                  <LogOutIcon iconFill={colorClasses} />
+                  <LogOutIcon iconFill={textClasses} />{' '}
+                  {/* Se pasa la clase dinámica */}
                 </span>
-                <span className="nav-label" style={{ color: textColor }}>
-                  Logout
-                </span>
+                <span className="nav-label">Logout</span>
               </NavLink>
             </li>
           </ul>
         </nav>
       </aside>
-      <div className="content">
+      <div className="content h-full">
         <Outlet />
       </div>
     </>
