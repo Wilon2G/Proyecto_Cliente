c3489c9f712ea5919b448051e14937689318bdea
Favio138 <44302490+Favio138@users.noreply.github.com>
2025-01-23 21:31:54 +0100

    fix

--- RetroFrog V4/app/models/user.server.ts
+++ RetroFrog V4/app/models/user.server.ts
@@ -4,7 +4,7 @@ import { getCurrentUser } from '~/utils/auth.server';
 export async function checkUser(username: string, password: string) {
   const user = await db.user.findUnique({
     where: {
-      userName: username,
+      username: username,
     },
   });
   if (!user) {
