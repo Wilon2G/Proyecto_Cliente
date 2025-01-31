622c7e92e2896e30eba71c90de8221b1729a372e
Guillermo <guilleleongordo@gmail.com>
2025-01-31 19:25:12 +0100

    contraseñas seguras y con hash, cuidado con los usuarios que tuvieseis creados porque ya no van a funcionar sus logins, mirad el grupo de wasap :)

--- RetroFrog V4/app/models/user.server.ts
+++ RetroFrog V4/app/models/user.server.ts
@@ -1,5 +1,7 @@
 import db from '~/db.server';
 import { getCurrentUser } from '~/utils/auth.server';
+import bcrypt from "bcryptjs";
+
 
 export async function checkUser(email: string, password: string) {
   const user = await db.user.findUnique({
@@ -10,11 +12,14 @@ export async function checkUser(email: string, password: string) {
   if (!user) {
     return null;
   }
-  if (user.password === password) {
+
+  //Tenemos contraseñas seguras hasheadas chiquis wiiiiiiii!!!! <3
+  const isValid = await bcrypt.compare(password, user.password);
+
+  if (isValid) {
     return user.id;
-  } else {
-    return null;
   }
+  return null;
 }
 
 export async function userExists(email: string) {
@@ -88,10 +93,14 @@ export async function createUser(userData: {
   name: string;
   email: string;
 }) {
+
+  const saltRounds = 10; // Esto es la cantidad de veces que se hashea la contraseña, lo he puesto a diez pero no sé si es mejor menos por tema velocidad...
+  const hashedPassword = await bcrypt.hash(userData.password, saltRounds);
+
   return db.user.create({
     data: {
       email: userData.email,
-      password: userData.password,
+      password: hashedPassword,
       name: userData.name,
       role: 'USER',
     },
@@ -110,3 +119,17 @@ export async function getUserFavGames(id: string) {
     },
   });
 }
+
+
+
+/*
+Notas para la presentación:
+En una contraseña hasheada por ejemplo:
+$2a$10$SJBIceDe1QldKzfarZPhKuk6af/KgLGejnz.N/53V62h43wpFSysu
+
+Lo que va después del primer $ es el algoritmo que se ha utilizado, en este caso el algoritmo bcrypt la versión a2.
+Lo siguiente $10 es el número de veces que se ha hasheado, esto lo hemos decidido antes en la variable saltRounds.
+Los siguientes 22 caracteres son la salt.
+Y por último, el resto es la contraseña hasheada.
+:)
+*/
