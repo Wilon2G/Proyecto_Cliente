55c3556b42b0ca9850a596a7145962c6bd18155a
Favio138 <44302490+Favio138@users.noreply.github.com>
2025-02-08 20:32:39 +0100

    test fix

--- RetroFrog_V4/tests/settings-page.spec.ts
+++ RetroFrog_V4/tests/settings-page.spec.ts
@@ -2,22 +2,43 @@ import { expect, test } from '@playwright/test';
 
 test.describe('Settings Page', () => {
   test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+    const emailInput = page.locator('input[name="emailLog"]');
+    await emailInput.waitFor();
+    await emailInput.fill('admin@example.com');
+    const passwordInput = page.locator('input[name="passwordLog"]');
+    await passwordInput.waitFor();
+    await passwordInput.fill('securepassword');
+    const loginButton = page.locator('button[name="_action"][value="logIn"]');
+    await loginButton.click();
+    await page.waitForURL('http://localhost:5173/home/main');
     await page.goto('http://localhost:5173/home/settings');
   });
 
-  test('has correct title', async ({ page }) => {
-    await expect(page).toHaveTitle('RetroFrog');
+  test('has correct URL', async ({ page }) => {
+    await expect(page).toHaveURL('http://localhost:5173/home/main');
   });
 
   test('can select background', async ({ page }) => {
-    const backgrounds = page.locator('input[name="background"]');
+    await expect(page).toHaveURL('http://localhost:5173/home/settings');
 
-    const count = await backgrounds.count();
-    if (count === 0) throw new Error('No se encontraron inputs de background');
+    const backgrounds = page.locator(
+      'input[name="background"][type="radio"][id="bg2"]',
+    );
 
-    await backgrounds
-      .first()
-      .evaluate((el) => (el as HTMLInputElement).click());
-    await expect(backgrounds.first()).toBeChecked();
+    await backgrounds.check;
+    const settingButton = page.locator(
+      'button[type="submit"]:has-text("Save Changes")',
+    );
+    await settingButton.waitFor({ state: 'visible' });
+    await settingButton.click();
+
+    const root = page.locator(':root');
+
+    const bgImageValue = await root.evaluate((el) =>
+      getComputedStyle(el).getPropertyValue('--bgImage').trim(),
+    );
+
+    expect(bgImageValue).toBe('url(/assets/background/3-bg.avif)');
   });
 });
